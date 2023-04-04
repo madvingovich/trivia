eb16bb98fa2a9d2294aa6ba01881e3b7db8b7a72
Вадим Логвинович <vadvoker@gmail.com>
2023-04-04 11:03:54 +0300

    quiz page desktop background

--- src/pages/QuizPage/components/Background/Background.tsx
+++ src/pages/QuizPage/components/Background/Background.tsx
@@ -7,14 +7,39 @@ import img3 from "images/bg-quiz-3.svg";
 import img4 from "images/bg-quiz-4.svg";
 import img5 from "images/bg-quiz-5.svg";
 
+import imgDesktop1 from "images/bg-quiz-desktop-1.svg";
+import imgDesktop2 from "images/bg-quiz-desktop-2.svg";
+import imgDesktop3 from "images/bg-quiz-desktop-3.svg";
+import imgDesktop4 from "images/bg-quiz-desktop-4.svg";
+
 function Background() {
   return (
     <div className={backgroundContainerStyle.container}>
-      <img alt="" src={img1} />
-      <img alt="" className={backgroundStyles.img2} src={img2} />
-      <img alt="" className={backgroundStyles.img3} src={img3} />
-      <img alt="" className={backgroundStyles.img4} src={img4} />
-      <img alt="" className={backgroundStyles.img5} src={img5} />
+      <div className={backgroundContainerStyle.mobileView}>
+        <img alt="" src={img1} />
+        <img alt="" className={backgroundStyles.img2} src={img2} />
+        <img alt="" className={backgroundStyles.img3} src={img3} />
+        <img alt="" className={backgroundStyles.img4} src={img4} />
+        <img alt="" className={backgroundStyles.img5} src={img5} />
+      </div>
+      <div className={backgroundContainerStyle.desktopView}>
+        <img alt="" src={imgDesktop1} />
+        <img
+          alt=""
+          className={backgroundStyles.imgDesktop2}
+          src={imgDesktop2}
+        />
+        <img
+          alt=""
+          className={backgroundStyles.imgDesktop3}
+          src={imgDesktop3}
+        />
+        <img
+          alt=""
+          className={backgroundStyles.imgDesktop4}
+          src={imgDesktop4}
+        />
+      </div>
     </div>
   );
 }

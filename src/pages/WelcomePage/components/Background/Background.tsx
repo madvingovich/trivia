fcb857b40ccd0856e3ec63e3e33a0c75c836a07b
Вадим Логвинович <vadvoker@gmail.com>
2023-04-04 10:56:05 +0300

    welcome desktop background view

--- src/pages/WelcomePage/components/Background/Background.tsx
+++ src/pages/WelcomePage/components/Background/Background.tsx
@@ -4,15 +4,40 @@ import img1 from "images/bg-welcome-1.svg";
 import img2 from "images/bg-welcome-2.svg";
 import img3 from "images/bg-welcome-3.svg";
 
+import imgDesktop1 from "images/bg-welcome-desktop-1.svg";
+import imgDesktop2 from "images/bg-welcome-desktop-2.svg";
+import imgDesktop3 from "images/bg-welcome-desktop-3.svg";
+import imgDesktop4 from "images/bg-welcome-desktop-4.svg";
+
 function Background() {
   return (
     <div
       className={`${backgroundContainerStyle.container} ${backgroundStyles.container}`}
     >
-      <img alt="" src={img1} />
-      <img alt="" className={backgroundStyles.img2} src={img2} />
-      <img alt="" className={backgroundStyles.img3} src={img3} />
-      <img alt="" className={backgroundStyles.img4} src={img1} />
+      <div className={backgroundContainerStyle.mobileView}>
+        <img alt="" src={img1} />
+        <img alt="" className={backgroundStyles.img2} src={img2} />
+        <img alt="" className={backgroundStyles.img3} src={img3} />
+        <img alt="" className={backgroundStyles.img4} src={img1} />
+      </div>
+      <div className={backgroundContainerStyle.desktopView}>
+        <img alt="" src={imgDesktop1} />
+        <img
+          alt=""
+          className={backgroundStyles.imgDesktop2}
+          src={imgDesktop2}
+        />
+        <img
+          alt=""
+          className={backgroundStyles.imgDesktop3}
+          src={imgDesktop3}
+        />
+        <img
+          alt=""
+          className={backgroundStyles.imgDesktop4}
+          src={imgDesktop4}
+        />
+      </div>
     </div>
   );
 }
